c7257502cc365cad5a2596b9e2c4333a61e37e2a
nalinbot <nalinnagar1@gmail.com>
2021-01-06 10:41:40 -0500

    Update Commit :rocket:
    Things working more smoothly now

---
+++ face_rec_example.py
@@ -0,0 +1,66 @@
+import face_recognition
+import os
+import cv2
+
+KNOWN_FACES_DIR = '/Users/ashishnagar/Desktop/image_rec/know_faces'
+UNKNOWN_FACES_DIR = '/Users/ashishnagar/Desktop/image_rec/unknown_faces'
+TOLERANCE = 0.6
+FRAME_THICKNESS = 3
+FONT_THICKNESS = 2
+MODEL = 'hog'
+
+print("loading known faces...")
+
+known_faces = []
+known_names = []
+
+def name_to_color(name):
+    # Take 3 first letters, tolower()
+    # lowercased character ord() value rage is 97 to 122, substract 97, multiply by 8
+    color = [(ord(c.lower())-97)*8 for c in name[:3]]
+    return color
+
+for name in os.listdir(KNOWN_FACES_DIR):
+    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
+        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+        encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append(encoding)
+        known_names.append(name)
+
+print('processing unknown faces...')
+
+for filename in os.listdir(UNKNOWN_FACES_DIR):
+    print(filename)
+    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+    locations = face_recognition.face_locations(image, model=MODEL)
+    encodings = face_recognition.face_encodings(image, locations)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+    for face_encoding, face_location in zip(encodings, locations):
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+        match = None
+        if True in results:
+            match = known_names[results.index(True)]
+            print(f"Match found: {match}")
+
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+            color = name_to_color(match)
+
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2]+22)
+
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+            cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+    cv2.imshow(filename, image)
+    cv2.waitKey(10000)
+    #cv2.destroyWindow(filename)
+            
+
+
+
+
+
